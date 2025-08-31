0a123098a9354c6f0d49ea2c12de0fa4d45f8985
Shahin <muhammedshahinjaleel@gmail.com>
2025-08-31 19:28:39 +0530

    Update imgTOascii.py

--- imgTOascii.py
+++ imgTOascii.py
@@ -1,120 +1,62 @@
-import random
-import string
+from PIL import Image
+import tkinter as tk
+from tkinter import filedialog
 
-targetWord = input("Enter a word: ").lower()
-word_length = len(targetWord)
+raw = " "
+root = tk.Tk()
+root.title("IMAGE TO ASCII RENDERER by Shahin")
+root.geometry("600x600")
+root.configure(bg="black")
 
-population = 100
-max_gens = 30000
+charsList = []
 
-mutation_rate = 0.3
-crossover_rate = 0.5
+raw = filedialog.askopenfilename(title="Pick a file: ", filetypes=[(".jpg",".png")])
 
-generation = []
-selectedSet = []
-fitnessScore = 0
-generation_number = 0
-best_guess = ""
-best_score = -1
-top_two = ["",""]
+def render(inputPath):
+      charsList.clear()
+      print("beginning render")
+      img = Image.open(str(inputPath))
+      width, height = img.size
+      x = width
+      y = height
 
-def initiation():
-    print("Population: " + str(population))
-    print("Max no of gens allowed: "+ str(max_gens))
-    print("mutation rate: " + str(mutation_rate))
-    print("crossover rate: " + str(crossover_rate))
-    for c in range(word_length):
-        fitness.append(0)
-    for _ in range(population):
-        letters = [random.choice(string.ascii_lowercase) for _ in range(word_length)]
-        curr_guess = ''.join(letters)
-        generation.append(curr_guess)
-    print("FIRST GEN: " + str(generation))
+      comp_factorX = 1
+      comp_factorY = 1
+      gray = img.convert('L')
 
-initiation()
+      
+      for j in range(y):
+            if j%comp_factorY== 0:
+                  for i in range(x):
+                        if i%comp_factorX== 0:
+                              pixel_value = gray.getpixel((i, j))
 
-def mutate(population, generation):
-    for p in range(population):
-            old_word = generation[p]
-            letters = []
-            for l in range(word_length):
-                if random.random() >= mutation_rate:
-                    letters.append(old_word[l])
-                else:
-                    letters.append(random.choice(string.ascii_lowercase))
-            generation[p] = ''.join(letters)
+                              chars = " .:-=+*#%@"
+                              ascii_char = chars[pixel_value * len(chars) // 256]
+                              charsList.append(ascii_char)
 
-def cross(tt):
-    split_point = int(round(word_length/2))
-    p1 = tt[0]
-    p2 = tt[1]
+                  charsList.append("\n")           
+      
+      print("ASCII art created successfully!")
 
-    c1 = p1[:split_point] + p2[split_point:]
+      asciiString = " ".join(charsList)
+      label = tk.Label(root, padx= 20, pady=10, text = asciiString, bg="black",fg= "white", font=('Ariel',1) )
+      label.pack()      
+      
 
-    return c1
+print("path selected")
+inputPath = raw
+filepathlabel = tk.Label(root,text=raw, padx = 30,height=2, font=('Ariel', 19))
+filepathlabel.pack()
 
-def runLoop():
-    global generation_number
-    global generation
-    global best_guess
-    while best_guess != targetWord and generation_number < max_gens:
-        mutate(population, generation)
-        calculateFitness()
-        generation_number += 1
-        #selectedSet.clear()
-
-    
-def calculateFitness():
-    global best_score
-    global best_guess
-    global top_two
-    global fitnessScore
-    for i,ind in enumerate(generation):
-        score = 0
-        
-        for id,c in enumerate(ind):
-            if targetWord[id] == c:
-                score += 1
-        
-        if score > best_score:
-            
-            best_score = score
-            best_guess = ind
-            if random.random() < crossover_rate:
-                for t, tt in enumerate(top_two):
-                    if tt != "":
-                        top_two[t] = best_guess
-                    else:
-                        top_two.append(best_guess)
-                
-                    ind = cross(top_two)
-            
-            if ind not in selectedSet:
-                selectedSet.append(ind)
-        fitnessScore = best_score
-
-def printGuess():
-    print("CURRENT: " +str(generation))
-
-runLoop()
-
-final_gen = []
-for l, lg in enumerate(generation):
-    if lg == best_guess:
-        final_gen.append("**" + lg.upper() + "**")
-    else:
-        final_gen.append(lg)
-
-print("LAST GEN: " + str(final_gen))
-
-print("BEST GUESS: " + best_guess)
-print("FITNESS: " + str(fitnessScore))
-print("Generations taken: " + str(generation_number))
-
-    
+def getInput():
+      render(inputPath)
+      
+convertButton = tk.Button(root, text= "CONVERT", command=lambda: getInput())
+convertButton.pack()
 
 
-        
-    
+root.mainloop()
 
-    
+
+
